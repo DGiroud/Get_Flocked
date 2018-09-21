58774e89fbfef31d424375c9eb74f0aca6d21c4c
Jake-Ailey <36465180+Jake-Ailey@users.noreply.github.com>
2018-09-21 16:53:15 +1000

    Added Jake's Scene, as well as BaseAnimal script and the unfinished Sheep Script.

---
+++ Assets/Scripts/BaseAnimal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAnimal : MonoBehaviour
+{    
+
+    private Vector3 position;
+
+    //Base function to set the position of any inheriting animal objects
+    // You've 2 options here, either pass in each dimension manually, or pass in an already created transform
+    // When using either of these 2 functions, you MUST pass in the animal being changed by using the "this" keyword as the first paramater
+    public void SetPosition(GameObject animal, float x, float y, float z)
+    {
+        animal.transform.position.Set(x, y, z); //Setting the animal's position using the passed in parameters.
+    }
+
+    //Basically the exact same as above, yet may be easier to use when scripting. Options are nice
+    public virtual void SetPosition(GameObject animal, Transform transform)
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+        animal.transform.position.Set(x, y, z);
+    }
+
+    //Base function to get the position of any inheriting animal objects, returns a float
+    public virtual Vector3 GetPosition(GameObject animal)
+    {
+        return animal.transform.position;
+    }
+
+    //Destroys the object when called
+    public virtual void Destroy(GameObject animal)
+    {
+        Destroy(animal);
+    }
+}
---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : BaseAnimal {
+
+    [Header("Sheep Tier Prefabs")]
+    public GameObject smallSheep;
+    public GameObject mediumSheep;
+    public GameObject largeSheep;
+
+    [Header("Sheep Modifiers")]
+    [Range(0, 500)]
+    [Tooltip("The Sheep's base movement speed")]
+    public float speed;
+
+    [Range(0, 500)]
+    [Tooltip("The weight of the sheep, affects the speed at which it can be pushed around by the player")]
+    public float mass;
+
+    [Tooltip("The duration that the sheep will remain idle before continuing it's wander movement")]
+    public float idleDuration;
+    [Tooltip("The duration that the sheep will wander around before continuing it's idle animation")]
+    public float wanderDuration;
+
+    //Multipliers for patterned sheep
+    [Tooltip("The varying score multipliers to accompany the different patterned sheep")]
+    public float[] scoreMultiplier = new float[4] {1.0f, 1.5f, 2.0f, 3.0f};
+    [Tooltip("Added possibility for different tiered sheep to have different growth times")]
+    public float[] growthTime;
+
+    //Determines the size of the sheep
+    int sizeTier;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Testing of Grow function
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            Grow();
+        }
+	}
+
+    //Whilst the sheep is undisturbed by a player, it will wander around consuming grass. When it does so, it periodically grows in size
+    private void WanderBehaviour()
+    {
+
+    }
+
+    //Function to upgrade the sheep to the next tier
+    private void Grow()
+    {
+               
+    }
+}
