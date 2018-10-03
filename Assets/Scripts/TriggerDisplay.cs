42b972ea318891a0d2448180048cf65a828e225e
Jake-Ailey <36465180+Jake-Ailey@users.noreply.github.com>
2018-10-03 16:06:52 +1000

    Jakes Commit, committing the Collider Box visual representation

---
+++ Assets/Scripts/TriggerDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TriggerDisplay : MonoBehaviour {
+
+    public Color mainColour = Color.red;
+    public Color lineColour = Color.red;
+
+    //Draws a box only visible in editor view
+    void OnDrawGizmos()
+	{
+		GetComponent <BoxCollider> ().isTrigger = true;
+		Vector3 drawBoxVector = new Vector3(
+			this.transform.lossyScale.x * this.GetComponent<BoxCollider>().size.x,
+			this.transform.lossyScale.y * this.GetComponent<BoxCollider>().size.y,
+			this.transform.lossyScale.z * this.GetComponent<BoxCollider>().size.z
+		);
+
+		Vector3 drawBoxPosition = this.transform.position + this.GetComponent<BoxCollider>().center;
+
+		Gizmos.matrix = Matrix4x4.TRS(drawBoxPosition,transform.rotation, drawBoxVector);
+		Gizmos.color = mainColour;
+		Gizmos.DrawCube (Vector3.zero, Vector3.one);
+		Gizmos.color = lineColour;
+		Gizmos.DrawWireCube (Vector3.zero, Vector3.one);
+	}
+}
