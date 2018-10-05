83824f13517f4dc77c6eeea00317520e87ed80fe
Jake-Ailey <36465180+Jake-Ailey@users.noreply.github.com>
2018-10-05 16:54:24 +1000

    Sheep movement is fixed

--- Assets/Scripts/Animal/Sheep/Sheep.cs
+++ Assets/Scripts/Animal/Sheep/Sheep.cs
@@ -34,14 +34,26 @@ public class Sheep : MonoBehaviour {
     private SheepTier currentTier { get; set; }
 
     [Header("A.I settings")]
-    public float wanderDuration;            //How long the sheep will wander around aimlessly
+    public float wanderDuration = 5.0f;            //How long the sheep will wander around aimlessly
+    public float idleMinRange;
+    public float idleMaxRange;
+    private float wanderTimer = 0f;                //Timer that will trigger the Wander state's GetNewDestination() function
     [Tooltip("Affects the interval between when a sheep will seek out a new position on the field. 1 = 1 second.")]
-    public int idleDuration = 10;
-    public float rotationSpeed = 10.0f;
+    public float rotationSpeed = 10.0f;             //The speed at which the sheep will spin or "Roll"
+    [Tooltip("The range that the agent needs to be in of it's desired location before it hits it's idle state")]
+    public float destCheckRadius;
+    private float randomNumGen;
 
     private float growthTimer = 0.0f;            //What will be incremented as the sheep idles, will be checked against growthRequirements
-    private int currentLevel = 0;           //Reference to the current level the game is taking place in
+    private int currentLevel = 0;                //Reference to the current level the game is taking place in
 
+    //Private variables so that we can peak into what an agent's destination is during runtime
+    private float agentDestinationX;
+    private float agentDestinationZ;
+
+    //*************************************************************************************************************************************
+    //ADD A FUNCTION THAT CHECKS IF THE OBJECT THIS IS ATTACHED TO HAS A NAV MESH AGENT COMPONENT, AND IF FALSE, CREATE ONE FOR THIS OBJECT
+    //*************************************************************************************************************************************
 
     // Use this for initialization
     void Start() {
@@ -54,6 +66,7 @@ public class Sheep : MonoBehaviour {
         agent.destination = fieldObject.transform.position;
 
         GetNewDestination();
+
 
         SetState(SheepState.Spawn);
 
@@ -73,8 +86,11 @@ public class Sheep : MonoBehaviour {
                 //When the sheep spawns, we don't want it to have to wait before finding a new location, hence we immediately have it
                 // calculate a new random position to seek to
                                  
-                if(transform.position == agent.destination)
+                if((transform.position.x <= agent.destination.x + destCheckRadius || transform.position.x >= agent.destination.x - destCheckRadius) ||
+                   (transform.position.z <= agent.destination.z + destCheckRadius || transform.position.z >= agent.destination.z - destCheckRadius))
                 {
+                    Debug.Log(currentState);
+
                     SetState(SheepState.Idle);
                 }                
 
@@ -93,13 +109,16 @@ public class Sheep : MonoBehaviour {
                 {
                     LevelUp();
                     growthTimer = 0.0f;
-                }                
+                }
 
-                //If the sheep has remained idle for the specified amount of time, seek a new location
-                if(timer >= idleDuration)
+                //If the sheep has remained idle for an amount of time (determined randomly between the specified range), seek a new location
+                if (timer > randomNumGen)
                 {
+                    GetNewDestination();
+
                     SetState(SheepState.Wander);
                     timer = 0f;
+                    randomNumGen = UnityEngine.Random.Range(idleMinRange, idleMaxRange);
                 }
 
                 //Add animations here
@@ -110,7 +129,20 @@ public class Sheep : MonoBehaviour {
 
             case SheepState.Wander:
 
-                GetNewDestination();
+                wanderTimer += Time.deltaTime;
+                
+                if (transform.position.x == agent.destination.x && transform.position.z == agent.destination.z)
+                { 
+                    SetState(SheepState.Idle);
+                    break;
+                }
+
+                //If the sheep gets stuck in a wander for too long, we force it into Idle
+                else if (wanderTimer >= 10.0f)
+                {
+                    SetState(SheepState.Idle);
+                    break;
+                }
 
                 //Add animations here (Same as Spawn)
 
@@ -160,24 +192,26 @@ public class Sheep : MonoBehaviour {
         float fieldPosX = fieldObject.transform.position.x;
         float fieldPosZ = fieldObject.transform.position.z;
 
-
+        #region Comments
         //Here we are getting the current field's position (as it may change in size through playtesting) and setting a new random point
         // within the field limits by getting the localScale (size) and halving it, which will give us the correct dimensions in all directions.
         // E.G. If the width of the field is 25, we want 12.5 from the center going in both directions, so we would find a random point in a range
         // between -12.5 and 12.5;
+        #endregion
         newPos.x = UnityEngine.Random.Range(fieldPosX - fieldObject.transform.localScale.x / 2.5f,
                                             fieldPosX + fieldObject.transform.localScale.x / 2.5f);
 
         newPos.z = UnityEngine.Random.Range(fieldPosZ - fieldObject.transform.localScale.z / 2.5f,
                                             fieldPosZ + fieldObject.transform.localScale.z / 2.5f);
 
+        agentDestinationX = newPos.x;
+        agentDestinationZ = newPos.z;
+
         agent.destination = newPos;
 
-        Debug.Log(agent.destination);
-
-        RotateTowards(newPos);
-
-        //WHEN THE SHEEP IS MOVING, YOU NEED TO USE IT'S CURRENT VELOCITY TO AFFECT IT'S ROTATION
+        //This function is not doing as intended. It is correctly changing the rotations inline with the sheep's
+        // movements, however it is not changing the rotation of the sheep model
+        RotateTowards(newPos); 
     }
 
     private void RotateTowards(Vector3 destination)
@@ -198,6 +232,3 @@ public class Sheep : MonoBehaviour {
         transform.localScale = new Vector3(size, size, size);
     }
 }
-
-
-//GET CURRENT POSITION, then se that fields current position as a base, and plus/minus 12.5f from that to get a position within the field
