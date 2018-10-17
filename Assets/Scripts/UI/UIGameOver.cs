bca6633c221065c55eb1435cd8c07393247ed6ef
Dillon Handyside <dj_handyside@hotmail.com>
2018-10-17 11:29:54 +1100

    Implemented GameOver menu. Adjusted UI scripts to be a little cleaner

---
+++ Assets/Scripts/UI/UIGameOver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGameOver : MonoBehaviour
+{
+    [SerializeField]
+    private Object game;
+    [SerializeField]
+    private Object mainMenu;
+
+    /// <summary>
+    /// loads the main game
+    /// </summary>
+    public void Replay()
+    {
+        SceneManager.LoadScene(game.name);
+    }
+
+    /// <summary>
+    /// loads the main menu
+    /// </summary>
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenu.name);
+    }
+
+    /// <summary>
+    /// forces application quit
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/UI/UIStartMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIStartMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Object game;
+
+    /// <summary>
+    /// loads the main game
+    /// </summary>
+    public void Play()
+    {
+        SceneManager.LoadScene(game.name);
+    }
+
+    /// <summary>
+    /// opens the settings menu
+    /// </summary>
+    public void Settings()
+    {
+
+    }
+
+    /// <summary>
+    /// forces application quit
+    /// </summary>
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
