11b607fdf348ce0baaf7623dcec0c2f30b639f2a
DGiroud <39175181+DGiroud@users.noreply.github.com>
2018-10-19 12:00:37 +1100

    Fixed the player prefab

--- Assets/Scripts/Player/Interaction.cs
+++ Assets/Scripts/Player/Interaction.cs
@@ -12,21 +12,27 @@ public class Interaction : MonoBehaviour
     /// <param name="other">the colliding object</param>
     private void OnTriggerEnter(Collider other)
     {
+		
+
         // if the colliding object isn't a sheep, ignore it
-        if (!other.CompareTag("Sheep"))
-            return;
+		if (other.tag != ("Sheep")) {
+			Debug.Log ("1");
+			return;
+		}
 
         BaseActor playerScript = GetComponentInParent<BaseActor>();
 
         // if the sheep is already being pushed then it can't be picked up
         if (other.GetComponent<Animator>().GetBool("isPushed"))
         {
+			Debug.Log ("2");
             playerScript.interactionSheep = other.gameObject;
             return;
         }
 
         // snap the sheep to actor
         playerScript.SnapSheep(other.gameObject);
+		Debug.Log ("3");
     }
 
     /// <summary>
